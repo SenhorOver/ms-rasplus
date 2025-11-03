1d9ab7b6585b178d688f0ca41047fada4c5ab1ff
Marcos V Silva <mrcsvs@outlook.com>
2025-11-03 17:22:45 -0300

    feat: implement email sender on listener

--- rasplus-message-api/src/main/java/com/client/rasplus/message/api/component/AMQPListener.java
+++ rasplus-message-api/src/main/java/com/client/rasplus/message/api/component/AMQPListener.java
@@ -1,15 +1,20 @@
 package com.client.rasplus.message.api.component;
 
 import com.client.rasplus.message.api.dto.UserRecoveryCodeDto;
+import com.client.rasplus.message.api.integration.MailIntegration;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AMQPListener {
 
+    @Autowired
+    private MailIntegration mailIntegration;
+
     @RabbitListener(queues = "recovery.code.email")
     public void recoveryCodeListener(UserRecoveryCodeDto dto) {
-        System.out.println(dto);
+        mailIntegration.send(dto.email(), dto.code(), "Código de Recuperação de conta");
     }
 
 }
