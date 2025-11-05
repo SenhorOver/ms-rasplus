d94aedaab5a29b1a948ba25d54f3f226245f18ee
Marcos V Silva <mrcsvs@outlook.com>
2025-11-05 17:13:48 -0300

    chore: create config server microservice

---
+++ rasplus-config-server/src/main/java/com/rasplus/client/configserver/RasplusConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.rasplus.client.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RasplusConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RasplusConfigServerApplication.class, args);
+	}
+
+}
