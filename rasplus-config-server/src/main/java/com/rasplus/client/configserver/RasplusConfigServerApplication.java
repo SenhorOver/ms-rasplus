dc6c04c795f7fbb9d3036332014246002c97fab8
Marcos V Silva <mrcsvs@outlook.com>
2025-11-05 17:23:40 -0300

    chore: configure config server

--- rasplus-config-server/src/main/java/com/rasplus/client/configserver/RasplusConfigServerApplication.java
+++ rasplus-config-server/src/main/java/com/rasplus/client/configserver/RasplusConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.rasplus.client.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class RasplusConfigServerApplication {
 
 	public static void main(String[] args) {
