fe722e32586d2978a269c65825aed0e8e9a4e962
Marcos V Silva <mrcsvs@outlook.com>
2025-11-06 18:15:38 -0300

    chore: configure spring cloud label and enable refresh

--- rasplus-message-api/src/main/java/com/client/rasplus/message/api/RasplusMessageApiApplication.java
+++ rasplus-message-api/src/main/java/com/client/rasplus/message/api/RasplusMessageApiApplication.java
@@ -2,8 +2,10 @@ package com.client.rasplus.message.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class RasplusMessageApiApplication {
 
 	public static void main(String[] args) {
